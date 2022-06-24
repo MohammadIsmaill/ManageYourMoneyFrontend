ee8cee0af4b5f0625efc3065f78f17779483402b
Mohammad Ismail <mohammad2002ismail@gmail.com>
2022-06-24 17:53:58 +0300

    added types for StatsTitleComponent

--- src/components/Stats/StatsTitle.tsx
+++ src/components/Stats/StatsTitle.tsx
@@ -1,5 +1,12 @@
-const StatsTitle = ({className,text}) => {
-    return <h1 className={className}>{text}</h1>
+import { FC } from "react"
+
+interface Props {
+  className: string
+  text: string
 }
 
-export default StatsTitle;
+const StatsTitle: FC<Props> = ({ className, text }) => {
+  return <h1 className={className}>{text}</h1>
+}
+
+export default StatsTitle

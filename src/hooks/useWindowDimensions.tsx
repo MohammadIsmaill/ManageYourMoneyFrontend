71414cdd5b74363ecea62088df76c913d63c9f6a
Mohammad Ismail <mohammad2002ismail@gmail.com>
2022-06-24 22:46:46 +0300

    added useWindowDimensions custom hook

---
+++ src/hooks/useWindowDimensions.tsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react"
+
+const useWindowDimensions = () => {
+  const hasWindow = typeof window !== "undefined"
+  function getWindowDimensions() {
+    const width = hasWindow ? window.innerWidth : null
+    const height = hasWindow ? window.innerHeight : null
+    return {
+      width,
+      height
+    }
+  }
+  function handleResize() {
+    setWindowDimensions(getWindowDimensions())
+  }
+
+  const [WindowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  )
+  useEffect(() => {
+    if (hasWindow) {
+      window.addEventListener("resize", handleResize)
+      return () => window.removeEventListener("resize", handleResize)
+    }
+  }, [hasWindow])
+  return WindowDimensions
+}
+
+export default useWindowDimensions
